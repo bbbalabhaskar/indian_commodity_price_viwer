b473132f2aa97e6620e6ffa202a98f471d0d52c7
Bala Bhaskar M <balabhaskar@live.com>
2019-06-02 00:19:59 +0530

    -- Market locaiton identification service
    Signed-off-by: Bala Bhaskar M <balabhaskar@live.com>

---
+++ src/main/java/in/sarada/collector/priceanalyzer/shared/Geometry.java
@@ -0,0 +1,23 @@
+package in.sarada.collector.priceanalyzer.shared;
+
+public class Geometry {
+
+    private double lat;
+    private double lng;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+}
